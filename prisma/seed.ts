b6461643a40e9e9ec668973f1b338f255ee0d3e6
Guuzta <Guuhsilva13@outlook.com>
2026-04-02 18:36:44 -0300

    criando seed para admin

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { prisma } from "../src/lib/prisma";
+import passwordHasher from "../src/utils/Password";
+
+import { env } from "../src/config/env";
+
+async function createAdmin() {
+  const adminExists = await prisma.user.findFirst({
+    where: { role: "ADMIN" },
+  });
+
+  if (adminExists) {
+    console.log("Admin already exists");
+    return;
+  }
+
+  const hashedPassword = await passwordHasher.hash(env.adminPassword);
+
+  await prisma.user.create({
+    data: {
+      name: "admin",
+      email: "admin@barbearia.com",
+      password: hashedPassword,
+      role: "ADMIN",
+    },
+  });
+
+  console.log("Admin created successfully");
+}
+
+createAdmin()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect());
--- src/config/env.ts
+++ src/config/env.ts
@@ -9,7 +9,12 @@ if (!process.env.TOKEN_EXPIRATION) {
   throw new Error("TOKEN_EXPIRATION not defined");
 }
 
+if (!process.env.ADMIN_PASSWORD) {
+  throw new Error("ADMIN_PASSWORD not defined");
+}
+
 export const env = {
   tokenSecret: process.env.TOKEN_SECRET,
   jwtOptions: { expiresIn: process.env.TOKEN_EXPIRATION } as SignOptions,
+  adminPassword: process.env.ADMIN_PASSWORD,
 };
