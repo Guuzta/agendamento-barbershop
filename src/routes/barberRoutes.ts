b7c0d7bc206d9ace3fd3b784b78ae20cd7d3a68f
Guuzta <Guuhsilva13@outlook.com>
2026-04-02 17:48:59 -0300

    adicionando middleware JWT nas rotas barber

--- src/routes/barberRoutes.ts
+++ src/routes/barberRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import BarberController from "../controllers/BarberController";
+import requireAuth from "../middlewares/requireAuth";
 
 const router = Router();
 
-router.get("/", BarberController.listAll);
-router.get("/:id", BarberController.getBarberById);
-router.get("/:id/availability", BarberController.getBarberAvailability);
+router.get("/", requireAuth, BarberController.listAll);
+router.get("/:id", requireAuth, BarberController.getBarberById);
+router.get("/:id/availability", requireAuth, BarberController.getBarberAvailability);
 
 export default router;
